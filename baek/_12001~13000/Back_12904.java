00b4e7b4eabf6c6f5941c38c64c2328107a19ba6
johyeoncheol <50797070+johyeoncheol@users.noreply.github.com>
2021-10-18 08:44:49 +0900

    백준 12904 A와 B

---
+++ baek/_12001~13000/Back_12904.java
@@ -0,0 +1,28 @@
+package com.Back;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back_12904 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder s = new StringBuilder(br.readLine());
+		StringBuilder t = new StringBuilder(br.readLine());		
+		while(s.length()<t.length()) {
+			if(t.charAt(t.length()-1)=='A') {
+				t.deleteCharAt(t.length()-1);
+			}else if(t.charAt(t.length()-1)=='B') {
+				t.deleteCharAt(t.length()-1);
+				t.reverse();
+			}
+		}
+		
+		if(t.toString().equals(s.toString())) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+	}
+}
