e14dc77c6f409dc1dbc2d129c83076cbd461a268
johyeoncheol <50797070+johyeoncheol@users.noreply.github.com>
2021-10-24 17:42:53 +0900

    백준 2206 벽 부수고 이동하기

--- baek/2001~3000/Back_2206.java
+++ baek/2001~3000/Back_2206.java
@@ -1,20 +1,18 @@
-package com.Back;
+package com.back;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Back_2206 {
-	static int N,M,arrSize,ans=Integer.MAX_VALUE;
-	static boolean visited[][][];
-	static int map[][];
-	static int dx[] = {-1,1,0,0};
-	static int dy[] = {0,0,-1,1};
-	static ArrayList<point> arr = new ArrayList<>();
+	static int N,M,ans=Integer.MAX_VALUE;
+	static int[][] map;
+	static int visited[][];
+	static int dx[]= {-1,1,0,0};
+	static int dy[]= {0,0,-1,1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,65 +21,66 @@ public class Back_2206 {
 		M = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][M];
+		visited = new int[N][M];
 		
+		String tmp;
 		for (int i = 0; i < N; i++) {
-			String tmp = br.readLine();
+			tmp = br.readLine();
 			for (int j = 0; j < M; j++) {
 				map[i][j]= tmp.charAt(j)-'0';
-				if(map[i][j]==1) {
-					arr.add(new point(i, j,0));
-				}
+				visited[i][j] = Integer.MAX_VALUE;
 			}
 		}
 		
-		visited = new boolean[N][M][arr.size()];
-		
-		for (int i = 0; i < arr.size(); i++) {
-			arrSize=i;
-			map[arr.get(i).x][arr.get(i).y]=0;
-			bfs();
-			map[arr.get(i).x][arr.get(i).y]=1;
-		}
-		if(ans==Integer.MAX_VALUE) {
-			System.out.println("-1");
-		}else {			
-			System.out.println(ans);
-		}
+		bfs();
+		if(ans == Integer.MAX_VALUE) System.out.println(-1);
+		else System.out.println(ans);
 	}
-
 	private static void bfs() {
-		Queue<point> q = new LinkedList<>();
-		visited[0][0][arrSize] = true;
-		q.add(new point(0,0,1));
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(0, 0, 1, 0));
+		visited[0][0] = 0;
+		
 		while(!q.isEmpty()) {
-			point p = q.poll();
-			if(p.x==N-1&&p.y==M-1) {
-				ans=Math.min(ans, p.cnt);
+			
+			Point p = q.poll();
+			
+			if(p.x == N-1 && p.y == M-1) {
+				ans = p.move;
 				break;
 			}
 			
-			for(int k = 0;k<4;k++) {
-				int xx = dx[k]+p.x;
-				int yy = dy[k]+p.y;
-				if(xx<0||xx>=N||yy<0||yy>=M)continue;
-				if(map[xx][yy]==0&&!visited[xx][yy][arrSize]) {
-					q.add(new point(xx,yy,p.cnt+1));
+			for (int i = 0; i < 4; i++) {
+				int xx = p.x + dx[i];
+				int yy = p.y + dy[i];
+				
+				if(xx<0 || xx>=N || yy<0 || yy>=M) continue;
+				
+				if(visited[xx][yy]<=p.des) continue;
+				
+				if(map[xx][yy]==0) {
+					visited[xx][yy] = p.des;
+					q.add(new Point(xx, yy, p.move+1, p.des));
+				}else {
+					if(p.des==0) {
+						visited[xx][yy]=p.des+1;
+						q.add(new Point(xx, yy, p.move+1, p.des+1));
+					}
 				}
-					
 			}
 		}
 	}
-
-	static class point{
+	static class Point{
 		int x;
 		int y;
-		int cnt;
-		public point(int x, int y,int cnt) {
+		int move;
+		int des;
+		public Point(int x, int y, int move, int des) {
 			super();
 			this.x = x;
 			this.y = y;
-			this.cnt=cnt;
+			this.move = move;
+			this.des = des;
 		}
-		
 	}
 }
